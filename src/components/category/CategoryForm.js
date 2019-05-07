4320fd973506e4e3a4be30f865d04277b1afa21c
cuppajake <cuppajake@gmail.com>
2019-05-07 13:44:53 -0400

    Setup CategoryList

---
+++ src/components/category/CategoryForm.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Container from 'react-bootstrap/Container';
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+
+import request from "../../utilities/api-request";
+
+class CategoryForm extends Component {
+  state = {
+    // TODO: implement initial state
+    name: '', 
+    disabled: true,
+  };
+
+  handleInputChange = event => {
+    // the value attribute of the input that was changed
+    const { value } = event.target;
+    // true or false based on whether the value is invalid
+    const disabled = value.length < 3;
+    // TODO: implement an expression that will set disabled based on the validity of the input value
+    this.setState({ name: value, disabled })
+    // TODO: update state with the new values of "disabled" and "name"
+  };
+
+  // sets the value to an empty string to reset the form
+  resetForm = () => this.setState({ name: '' });
+
+  handleSubmit = async event => {
+    event.preventDefault();
+    const { name } = this.state;
+    const { addCategory } = this.props;
+
+    // TODO: send a POST request with the form data (don't forget to await the Promise!)
+    const res = await request.post("/categories", { name });
+    const category = res.data;
+
+    addCategory(category)
+    // TODO: send the new category back to the <CategoriesView> Parent
+    this.resetForm();
+  };
+
+  render() {
+    const { disabled, name } = this.state;
+
+    return (
+      <Container className="text-center">
+        <h2>Create a Category</h2>
+        <Form>
+          <Form.Group as={Col} sm={{ offset: 4, span: 4 }}>
+            <Form.Label>Category Name</Form.Label>
+            <Form.Control
+              name="name"
+              minLength="3"
+              maxLength="15"
+              value={name}
+              onChange={this.handleInputChange}
+            />
+          </Form.Group>
+          
+          <Button
+            type="submit"
+            variant="primary"
+            onClick={this.handleSubmit}
+            disabled={disabled}
+            // attribute prop, which attribute?
+            // event prop, which event?
+            // TODO: implement the remaining props
+          >
+            Create
+          </Button>
+        </Form>
+        <hr />
+      </Container>
+    );
+  }
+}
+
+CategoryForm.propTypes = {
+  // TODO: implement the prop types for this component (see below)
+  addCategory: PropTypes.func.isRequired,
+  name: PropTypes.string,
+  disabled: PropTypes.bool,
+};
+
+export default CategoryForm;
--- src/views/CategoriesView.js
+++ src/views/CategoriesView.js
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { Component } from "react";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
 
-const CategoriesView = () => {
-  return <div>Categories View</div>;
+import request from "../utilities/api-request";
+import CategoriesList from "../components/category/CategoriesList";
+import CategoryForm from "../components/category/CategoryForm";
+
+class CategoriesView extends Component {
+  state = {
+    // TODO: declare initial state
+    categories: []
+  };
+
+  async componentDidMount() {
+    // request doesnt exist yet but we expect it to behave this way
+    // meaning execute an HTTP GET request at api-domain/categories
+    const res = await request.get("/categories");
+    const categories = res.data; // the response data is the category collection list
+    this.setState({ categories })
+    }
+
+    // TODO: implement updating state with the categories from the API
+  
+
+  addToCategories = category => {
+    this.setState(state => {
+      const { categories } = state;
+      // TODO: implement returning a new state with the category added to categories list
+      return { categories: [category, ...categories] };
+    })};
+
+  render() {
+    const { categories } = this.state;
+
+    return (
+      <Container>
+        <Row>
+          {/* TODO: declare the CategoryForm component and props */}
+          <CategoryForm categories={categories} addCategory={this.addToCategories}/>
+        </Row>
+        <br />
+        <Row>
+          {/* TODO: declare the CategoriesList component and props */}
+          <CategoriesList categories={categories}/>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default CategoriesView;
